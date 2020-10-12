183f3ce42ddfc78c3a082fc48fdc7fa6ef5cc999
AndyGeek <andy.eulogio@gmail.com>
2020-10-12 13:13:59 -0500

    Structure navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
+// FontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee, faTimes, faBars } from "@fortawesome/free-solid-svg-icons";
+
 import "./Navbar.css";
 
 export const Navbar = () => {
+   // Click del menu
+   const [click, setClick] = useState(false);
+
    return (
       <>
-         <Link to="/" exact>
-            Home
-         </Link>
-         <Link to="/services">Services</Link>
-         <Link to="/products">Products</Link>
-         <Link to="/sign-up">Sign-up</Link>
+         <div className="navbar">
+            <div className="navbar-container">
+               <Link to="/">
+                  AndyGeek
+                  <FontAwesomeIcon icon={faCoffee} />
+               </Link>
+               <div className="menu-icon">
+                  {click ? (
+                     <FontAwesomeIcon icon={faTimes} />
+                  ) : (
+                     <FontAwesomeIcon icon={faBars} />
+                  )}
+               </div>
+               <ul>
+                  <li className="nav-item">
+                     <Link to="/" exact>
+                        Home
+                     </Link>
+                  </li>
+
+                  <li className="nav-item">
+                     <Link to="/services">Services</Link>
+                  </li>
+
+                  <li className="nav-item">
+                     <Link to="/products">Products</Link>
+                  </li>
+
+                  <li className="nav-item">
+                     <Link to="/sign-up">Sign-up</Link>
+                  </li>
+               </ul>
+            </div>
+         </div>
       </>
    );
 };
