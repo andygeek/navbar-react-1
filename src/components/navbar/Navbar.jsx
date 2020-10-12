63cc0ad98a1b142fe6a9ae4e41684831c2fe7237
AndyGeek <andy.eulogio@gmail.com>
2020-10-12 18:14:15 -0500

    Improve styles with responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,39 +11,46 @@ export const Navbar = () => {
    // Click del menu
    const [click, setClick] = useState(false);
 
+   const handleClick = () => {
+      setClick(!click);
+   };
+
    return (
       <>
          <div className="navbar">
             <div className="navbar-container">
                <Link to="/" className="navbar-logo">
-                  Andy  
+                  Andy
                   <FontAwesomeIcon icon={faCoffee} />
                   Geek
                </Link>
-               <div className="menu-icon">
+               <div className="menu-icon" onClick={handleClick}>
                   {click ? (
                      <FontAwesomeIcon icon={faTimes} color="white" />
                   ) : (
-                     <FontAwesomeIcon icon={faBars}  color="white"/>
+                     <FontAwesomeIcon icon={faBars} color="white" />
                   )}
                </div>
-               <ul className="nav-menu">
+               <ul className={click ? "nav-menu active" : "nav-menu"}>
                   <li className="nav-item">
                      <Link to="/" className="nav-links">
                         Home
                      </Link>
                   </li>
-
                   <li className="nav-item">
-                     <Link to="/services" className="nav-links">Services</Link>
+                     <Link to="/services" className="nav-links">
+                        Services
+                     </Link>
                   </li>
-
                   <li className="nav-item">
-                     <Link to="/products" className="nav-links">Products</Link>
+                     <Link to="/products" className="nav-links">
+                        Products
+                     </Link>
                   </li>
-
                   <li className="nav-item">
-                     <Link to="/sign-up" className="nav-links">Sign-up</Link>
+                     <Link to="/sign-up" className="nav-links">
+                        Sign-up
+                     </Link>
                   </li>
                </ul>
             </div>
